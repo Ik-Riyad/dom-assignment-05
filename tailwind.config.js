383bc4ec5a6dbc10a55629f3e64c8098599f005e
Ibrahim Khan Riyadh <ik.riyad123@gmail.com>
2025-03-01 05:37:46 +0600

    frontend complete

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+<script>
+        tailwind.config = {
+          theme: {
+            extend: {
+              colors: {
+                clifford: '#da373d',
+                bcground: '#F4F7FF',
+              }
+            }
+          }
+        }
+      </script>
