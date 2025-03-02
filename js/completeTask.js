a5328a391baa494e93f40f6d06dd05f5817db1f2
Ibrahim Khan Riyadh <ik.riyad123@gmail.com>
2025-03-02 15:47:52 +0600

    working on external questins page

--- js/completeTask.js
+++ js/completeTask.js
@@ -1,20 +1,38 @@
 const cardsBtn = document.querySelectorAll(".card-btn");
+document.getElementById("day").innerText = `${dayName()},`;
+document.getElementById("date").innerText = currentDate();
 
+let clickedCount = 0;
 
 for (let i = 0; i < cardsBtn.length; i++) {
     const cardBtn = cardsBtn[i];
     cardBtn.addEventListener('click',
-        function () {
+        function (event) {
             alert("Board Updated Successfully");
             cardBtn.disabled = true;
             cardBtn.classList.add("bg-gray-200", "text-gray-400");
             const historyContainer = document.getElementById("history-container");
             const completeTaskContent = document.createElement("div");
+            let headerName = event.target.parentNode.parentNode.parentNode.firstChild.nextElementSibling.nextElementSibling.innerText;
             completeTaskContent.innerHTML = `
-            <p class="p-3 bg-bcground rounded-2xl mb-8">You have completed the task ${headerText()} at ${setTime()}</p>
+            <p class="p-3 bg-bcground rounded-2xl mb-8">You have completed the task ${headerName} at ${setTime()}</p>
             `
             historyContainer.appendChild(completeTaskContent);
+            const convertedSum = checkBoxSum("check-Box");
+            let sum = convertedSum + 1;
+            document.getElementById("check-Box").innerText = sum;
 
+            const convertedSubs = checkBoxSum("substraction-num");
+            let substraction = convertedSubs - 1;
+            document.getElementById("substraction-num").innerText = substraction;
+
+            clickedCount++;
+
+            if (clickedCount === cardsBtn.length) {
+                 {
+                    alert("Congrates!!! You have completed all the current task");
+                };
+            }
         })
 };
 document.getElementById("clear-history").addEventListener('click', function () {
--- js/function.js
+++ js/function.js
@@ -3,19 +3,25 @@ function setTime() {
     return times.toLocaleTimeString();
 }
 
-function checkBoxSum() {
-    const checkBox = document.getElementById("check-Box").innerText;
+function dayName() {
+    let daysName = ["Sunday", "Monday", "Twesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = new Date();
+    let weekDay = daysName[day.getDay()];
+    return weekDay;
+}
+function currentDate() {
+    let daysName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let month = new Date();
+    let monthName = daysName[month.getMonth()];
+    let dateName = month.getDate();
+    let yearName = month.getFullYear();
+
+    return `${monthName} ${dateName} ${yearName}`;
+}
+
+function checkBoxSum(id) {
+    const checkBox = document.getElementById(id).innerText;
     const number = parseInt(checkBox);
-    return number+1;
+    return number;
 }
-
-function headerText() {
-    let textStore = [];
-    const headerElements = document.querySelectorAll(".header-element");
-    for (const headerElement of headerElements) {
-        textStore.push(headerElement.innerText);
-    }
-    return textStore;
-}
-
 
