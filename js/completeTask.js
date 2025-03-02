80579929c1a9f6451d5b70c4c63604824de47353
Ibrahim Khan Riyadh <ik.riyad123@gmail.com>
2025-03-02 23:10:00 +0600

    added some comments

--- js/completeTask.js
+++ js/completeTask.js
@@ -1,12 +1,18 @@
+// Background Color change randomly
 document.getElementById("Background-btn").addEventListener('click', function () {
     document.getElementsByTagName("body")[0].style.background =
     "#" + bgColor();
 })
 
+// get the card button into a variable
 const cardsBtn = document.querySelectorAll(".card-btn");
+// set realtime day name using created function 
 document.getElementById("day").innerText = `${dayName()},`;
+
+// set realtime month date and year name using created function 
 document.getElementById("date").innerText = currentDate();
 
+// completed task alert var
 let clickedCount = 0;
 
 for (let i = 0; i < cardsBtn.length; i++) {
@@ -14,25 +20,35 @@ for (let i = 0; i < cardsBtn.length; i++) {
     cardBtn.addEventListener('click',
         function (event) {
             alert("Board Updated Successfully");
+
+            // disable the card with disabled true in button
             cardBtn.disabled = true;
             cardBtn.classList.add("bg-gray-200", "text-gray-400");
+
+            // called the history Container to append the created child div element
             const historyContainer = document.getElementById("history-container");
             const completeTaskContent = document.createElement("div");
+
+            // Retrieve the inner text by manually navigating the DOM tree using parentNode and firstChild.nextElementSibling.
             let headerName = event.target.parentNode.parentNode.parentNode.firstChild.nextElementSibling.nextElementSibling.innerText;
             completeTaskContent.innerHTML = `
             <p class="p-3 bg-bcground rounded-2xl mb-8">You have completed the task ${headerName} at ${setTime()}</p>
             `
             historyContainer.appendChild(completeTaskContent);
+
+            // Increase the number with Sum after clicked the button
             const convertedSum = checkBoxSum("check-Box");
             let sum = convertedSum + 1;
             document.getElementById("check-Box").innerText = sum;
 
+            //Decrease the number with subtraction after clicked the button
             const convertedSubs = checkBoxSum("substraction-num");
             let substraction = convertedSubs - 1;
             document.getElementById("substraction-num").innerText = substraction;
 
+            // completed task alert varName increase by clicked
             clickedCount++;
-
+            // completed task alert condition
             if (clickedCount === cardsBtn.length) {
                  {
                     alert("Congrates!!! You have completed all the current task");
@@ -40,6 +56,7 @@ for (let i = 0; i < cardsBtn.length; i++) {
             }
         })
 };
+// Clear history button
 document.getElementById("clear-history").addEventListener('click', function () {
     const historyContainer = document.getElementById("history-container");
     historyContainer.innerHTML = "";
--- js/function.js
+++ js/function.js
@@ -1,14 +1,16 @@
+// Real time function with AM and PM using toLocaleTimeString()
 function setTime() {
     let times = new Date();
     return times.toLocaleTimeString();
 }
-
+// Created the real Time day using getDay()
 function dayName() {
-    let daysName = ["Sunday", "Monday", "Twesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let daysName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     let day = new Date();
     let weekDay = daysName[day.getDay()];
     return weekDay;
 }
+// Current date 
 function currentDate() {
     let daysName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let month = new Date();
@@ -19,12 +21,14 @@ function currentDate() {
     return `${monthName} ${dateName} ${yearName}`;
 }
 
+// get the inner Text and converted to number data type and sum or subtraction with given number
 function checkBoxSum(id) {
     const checkBox = document.getElementById(id).innerText;
     const number = parseInt(checkBox);
     return number;
 }
 
+// background color function using random number to created a Hex color code
 function bgColor() {
     let hexNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",];
     let hexColorCode = "";
